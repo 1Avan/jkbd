35bf1c65dc921820e7ac873c14926f986154026b
流川枫 <1965564484@qq.com>
2024-03-28 19:51:39 +0800

    first commit

---
+++ src/components/Carousel.vue
@@ -0,0 +1,130 @@
+<template>
+    <div class="carousel-box">
+      <el-carousel height="380px" trigger="click" :interval="4000" pause-on-hover>
+        <el-carousel-item v-for="item in imgList" :key="item.id">
+            <a target="_blank" rel="nofollow" :href=item.href class="a-dis" 
+            :style="{ 
+                    // 'background': 'url('+item.url+') center center no-repeat',
+                    'background': 'url('+item.url+') center center no-repeat',
+                    'background-size': 'auto 100%'
+                }"
+            ></a>
+        </el-carousel-item>
+      </el-carousel>
+      <div class="menu-container">
+        <div class="banner-menu">
+            <p class="menu-title">学车流程</p>
+            <ul class="menu-list">
+                <li class="icon1">
+                    <p class="p1"><a target="_blank" href="https://www.jiakaobaodian.com/mnks/kemu1/car.html">科目一</a></p>
+                    <p class="p2">2024新题库</p>
+                </li>
+                <li class="icon2">
+                    <p class="p1"><a target="_blank" href="https://www.jiakaobaodian.com/mnks/kemu2/car.html">科目二</a></p>
+                    <p class="p2">精选视频，详解考点</p>
+                </li>
+                <li class="icon3">
+                    <p class="p1"><a target="_blank" href="https://www.jiakaobaodian.com/mnks/kemu3/car.html">科目三</a></p>
+                    <p class="p2">全方位讲解，攻克难点</p>
+                </li>
+                <li class="icon4">
+                    <p class="p1"><a target="_blank" href="https://www.jiakaobaodian.com/mnks/kemu4/car.html">科目四</a></p>
+                    <p class="p2">2024新题库</p>
+                </li>
+            </ul>
+        </div>
+      </div>
+
+
+    </div>
+  </template>
+  
+<style scoped lang="less">
+.menu-container {
+    position: relative;
+    width: 1200px;
+    height: 0;
+    margin: 0 auto;
+    overflow: visible;
+}
+.banner-menu{
+    position: absolute;
+    top: -358px;
+    left: 0;
+    width: 280px;
+    z-index: 1;
+    padding: 0 20px;
+    background-color: rgba(255, 255, 255, .9);
+    .menu-title {
+        line-height: 50px;
+        font-size: 20px;
+        border-bottom: 1px dotted #ddd;
+        text-align: center;
+    }
+    .menu-list {
+        
+        .icon1 {
+            background: url(../images/km1.png) left center no-repeat;
+        }
+        .icon2 {
+            background: url(../images/km2.png) left center no-repeat;
+        }
+        .icon3 {
+            background: url(../images/km3.png) left center no-repeat;
+        }
+        .icon4 {
+            background: url(../images/km4.png) left center no-repeat;
+        }
+        >li {
+            padding: 18px 20px 18px 50px;
+            .p1 {
+            color: #37B5F8;
+            font-size: 16px;
+            }
+            .p2 {
+                margin-top: 5px;
+                color: #666;
+            }
+        }
+    }
+}
+
+.a-dis {
+    display: inline-block;
+    width: 100%;
+    height: 380px;
+}
+</style>
+<script>
+export default{
+    name:"Carousel",
+    data(){
+        return{
+            imgList:[
+                {
+                    id:1,
+                    url:new URL('../images/carouselimg1.png',import.meta.url),
+                    href:"https://www.jiakaobaodian.com/download/"
+                },
+                {
+                    id:2,
+                    url:new URL('../images/carouselimg2.png',import.meta.url),
+                    href:"https://www.mucang.cn/company/honor/"
+                },
+                {
+                    id:3,
+                    url:new URL('../images/carouselimg3.png',import.meta.url),
+                    href:"https://www.mucang.cn/jiaolianbaodian/?from=jxrz-topbanner"
+                },
+                {
+                    id:4,
+                    url:new URL('../images/carouselimg4.png',import.meta.url),
+                    href:"https://saas-shop.jiakaobaodian.com/runtu/cooperate.html"
+                }
+            ]
+        }
+        
+    }
+}
+
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,219 @@
+<template>
+  <div>
+    <!-- 顶部区域 -->
+    <header class="header">
+      <div class="header-top">
+        <!-- logo区域 -->
+        <div class="logo-w" >
+          <a class="logo" href="https://www.jiakaobaodian.com/"></a>
+        </div>
+        <!-- 搜索框 -->
+        <div class="search-w">
+          <form action="https://www.so.com/s" target="_blank" id="so360form">
+            <input type="text" placeholder="请输入关键字" autocomplete="off" name="q" id="so360_keyword" class="search-input">
+            <input type="submit" class="button" ref="soSubmit" id="so360_submit" value="搜 索">
+            <input type="hidden" name="ie" value="utf8"><input type="hidden" name="src" value="zz_jiakaobaodian.com"><input type="hidden" name="site" value="jiakaobaodian.com">
+            <input type="hidden" name="rg" value="1">
+          </form>
+        </div>
+      </div>
+          <!-- 导航栏 -->
+      <nav class="horizontal-nav">
+        <ul class="nav-w">
+          <li><a @click="selectNavItem('home')" :class="{ 'active': selectedNavItem === 'home' }" href="https://www.jiakaobaodian.com/" >首页</a></li>
+          <li class="menu-jx" @click="selectNavItem('mnks')">
+            <a class="" href="https://www.jiakaobaodian.com/mnks/" :class="{ 'active': selectedNavItem === 'mnks' }">模拟考试</a>
+            <div class="drop-w">
+              <a href="https://www.jiakaobaodian.com/mnks/kemu1/car.html">科目一</a>
+              <a href="https://www.jiakaobaodian.com/mnks/kemu2/car.html">科目二</a>
+              <a href="https://www.jiakaobaodian.com/mnks/kemu3/car.html">科目三</a>
+              <a href="https://www.jiakaobaodian.com/mnks/kemu4/car.html">科目四</a>
+            </div>
+          </li>
+          <li class="menu-jx" @click="selectNavItem('school')" >
+            <a  href="https://www.jiakaobaodian.com/school/" :class="{ 'active': selectedNavItem === 'school' }">找驾校</a>
+            <div class="drop-w">
+              <a href="https://www.jiakaobaodian.com/school/">找驾校</a>
+              <a href="https://www.jiakaobaodian.com/coach/">找教练</a>
+              <a href="https://www.jiakaobaodian.com/peilian/">找陪练</a>
+            </div>
+          </li>
+          <li><a class="" href="https://www.jiakaobaodian.com/bbs/" @click="selectNavItem('bbs')" :class="{ 'active': selectedNavItem === 'bbs' }">驾考圈</a></li>
+          <li><a class="" href="https://www.jiakaobaodian.com/sign/" @click="selectNavItem('sign')" :class="{ 'active': selectedNavItem === 'sign' }">交通标志</a></li>
+          <li><a class="" href="https://www.jiakaobaodian.com/download/" @click="selectNavItem('download')" :class="{ 'active': selectedNavItem === 'download' }">软件下载</a></li>
+          <li class="qiye"><a href="https://qiye.jiakaobaodian.com" target="_blank" rel="nofollow">驾考宝典企业版</a></li>
+          <li><a href="https://www.futurephantom.cn/" target="_blank" rel="nofollow">智慧驾校</a></li>
+          <li class="menu-jx"><a href="https://www.maiche.com/rank/?from=jiakao_web" >买新车</a><div class="drop-w"><a href="https://www.maiche.com/rank/?from=jiakao_web" target="_blank" rel="nofollow">买车网</a><a href="http://www.pingxing.cn" target="_blank" rel="nofollow">平行之家</a></div></li>
+          <li><a href="http://www.xiaozhu2.com/?from=jiakao_web" target="_blank" rel="nofollow">二手车</a></li>
+        </ul>
+      </nav>
+
+    </header>
+
+
+    <!-- 轮播图 -->
+    <div class="carousel">
+      <Carousel/>
+    </div>
+  </div>
+</template>
+
+<script>
+// 导入轮播图组件
+import Carousel from './Carousel.vue';
+export default {
+  components:{
+    Carousel,
+  },
+  name: 'HelloWorld',
+  data(){
+    return{
+      selectedNavItem:""
+    }
+  },
+  methods:{
+    selectNavItem(item) {
+      this.selectedNavItem = item;
+    }
+  }
+  
+}
+</script>
+
+<style scoped lang="less">
+
+header {
+  height: 148px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  
+}
+header .header-top{
+  width: 1200px;
+  height: 42px;
+  margin: 28px auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .logo-w{
+    width: 207px;
+    height: 100%;
+    overflow: hidden;
+    font-size: 36px;
+    .logo{
+      display: block;
+      width: 207px;
+      height: 100%;
+      background: url(../images/logo.png) left center no-repeat;
+    }
+  }
+
+    .search-w{
+      position: relative;
+      width: 300px;
+      input{
+      float: left;
+      height: 42px;
+      width: 100%;
+      line-height: 40px;
+      border: 1px solid #e9e9e9;
+      font-size: 14px;
+      padding: 0 40px 0 10px;
+      }
+      
+      .button {
+        position: absolute !important;
+        top: 0 !important;
+        right: 0 !important;
+        width: 14% !important;
+      }
+
+  }
+
+    input:focus {
+    outline: none; 
+    border:1px solid #007aff; /* 输入框获得焦点时的边框颜色 */
+  }
+
+}
+
+.horizontal-nav{
+  width: 100%;
+  height: 50px;
+  background-color: #37B5F8;
+  color: #fff;
+  .nav-w{
+    width: 1200px;
+    margin: auto;
+    height: 50px;
+    line-height: 50px;
+    font-size: 16px;
+    text-align: left;
+    .qiye {
+      width: 148px;
+    }
+    .menu-jx {
+      position: relative;
+      overflow: visible;
+ 
+      >a:after{
+        display: inline-block;
+        vertical-align: middle;
+        width: 10px;
+        height: 10px;
+        margin-left: 6px;
+        margin-top: 5px;
+        border: 5px solid transparent;
+        border-top: 5px solid #fff;
+        content: ' ';
+      }
+      .drop-w{
+          font-size: 14px;
+          display: none;
+          position: absolute;
+          top: 50px;
+          width: 100%;
+          background-color: #fff;
+          z-index: 2;
+          line-height: 40px;
+          padding: 0 8px;
+          color: #666;
+          box-shadow: 0 0 20px rgba(0,0,0,.1);
+          >a:hover {
+            color: #37b5f9;
+          }
+      }
+    }
+
+    .menu-jx:hover .drop-w{
+      display: block;
+    }
+    >li:hover{
+      background-color:#54c0f9;
+    }
+
+
+    >li{
+      display: inline-block;
+      padding: 0;
+      vertical-align: top;
+      width: 108px;
+      text-align: center;
+      a {
+        display: block;
+        height: 100%;
+        width: 100%;
+      }
+
+      .active {
+        background-color: #258FC7;
+      }
+    }
+  }
+
+}
+
+
+</style>
